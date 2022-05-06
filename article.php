a8ccc30b94f64c06d5918e4b4e34b868e95d6fbb
Lilian Vasconcelos <lilianvscls@gmail.com>
2022-05-06 09:23:40 +0200

    dddr

--- article.php
+++ article.php
@@ -1,24 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<table>
-    <thead>
-        <tr>
-            <th colspan="2">Listes des articles</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>The table body</td>
-            <td>with two columns</td>
-        </tr>
-    </tbody>
-</table>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h2>Casque</h2>
+            </div>
+            <img src="https://joueclub-joueclub-fr-storage.omn.proximis.com/Imagestorage/imagesSynchro/0/0/359ee1919982d3eecc018c3edcbeb0f5a259e474_37000001.jpeg" alt="">
+            <div class="card-body">
+                <p class="description"> CAsque audio </p>
+                <span class="prix">56€</span>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h2>Casque</h2>
+            </div>
+            <img src="" alt="">
+            <div class="card-body">
+                <p class="description"> CAsque audio </p>
+                <span class="prix">56€</span>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h2>Casque</h2>
+            </div>
+            <img src="" alt="">
+            <div class="card-body">
+                <p class="description"> CAsque audio </p>
+                <span class="prix">56€</span>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h2>Casque</h2>
+            </div>
+            <img src="" alt="">
+            <div class="card-body">
+                <p class="description"> CAsque audio </p>
+                <span class="prix">56€</span>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h2>Casque</h2>
+            </div>
+            <img src="" alt="">
+            <div class="card-body">
+                <p class="description"> CAsque audio </p>
+                <span class="prix">56€</span>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h2>Casque</h2>
+            </div>
+            <img src="" alt="">
+            <div class="card-body">
+                <p class="description"> CAsque audio </p>
+                <span class="prix">56€</span>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h2>Casque</h2>
+            </div>
+            <img src="" alt="">
+            <div class="card-body">
+                <p class="description"> CAsque audio </p>
+                <span class="prix">56€</span>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
